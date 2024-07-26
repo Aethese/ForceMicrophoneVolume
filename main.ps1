87287a82e4438a1f29418799777737872cb8ebec
Aethese <59108437+Aethese@users.noreply.github.com>
2024-07-26 05:07:40 -0500

    remove test var

--- main.ps1
+++ main.ps1
@@ -1,8 +1,6 @@
 $scriptDir = Split-Path -parent $MyInvocation.MyCommand.Path
 Import-Module $scriptDir\Modules\AudioDeviceCmdlets\3.1.0.2\AudioDeviceCmdlets.psd1
 
-#$micID = "{0.0.1.00000000}.{5498d77b-7301-486e-8168-8c43a626ef60}"
-
 Write-Output "Select by Index which microphone you'd like to change:"
 Start-Sleep -Seconds 1
 Get-AudioDevice -List
