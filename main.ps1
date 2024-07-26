c222324b13241500e376554e304ac62abca38669
Aethese <59108437+Aethese@users.noreply.github.com>
2024-07-26 05:12:05 -0500

    set your own volume + more printed info
    print runtime and the user's volume level before loop runs

--- main.ps1
+++ main.ps1
@@ -1,6 +1,8 @@
 $scriptDir = Split-Path -parent $MyInvocation.MyCommand.Path
 Import-Module $scriptDir\Modules\AudioDeviceCmdlets\3.1.0.2\AudioDeviceCmdlets.psd1
 
+$volumeLevel = Read-Host "Your preferred volume level (as a percentage)"
+
 Write-Output "Select by Index which microphone you'd like to change:"
 Start-Sleep -Seconds 1
 Get-AudioDevice -List
@@ -15,14 +17,14 @@ try {
 }
 
 Set-AudioDevice -Id $deviceID # select mic
-Set-AudioDevice -RecordingCommunicationVolume 100 # change volume
-
-Write-Output "Setting microphone value to 100%. Press ctrl+c to stop the script"
+Set-AudioDevice -RecordingCommunicationVolume $volumeLevel # change volume
 
 $iteration = 0
 $runTime = 4 # in hours
+Write-Output "Setting microphone value to $volumeLevel% for $runTime hour(s). Press ctrl+c to stop the script early"
+
 while ($iteration -lt ($runTime * 60 / 0.5)) {
-	Set-AudioDevice -RecordingCommunicationVolume 100 # change volume
+	Set-AudioDevice -RecordingCommunicationVolume $volumeLevel
 	$iteration += 1
 	Start-Sleep -Milliseconds 500
 }
