5b94b6d4d1106ab2b5ff72230f19a944ce8c4866
Aethese <59108437+Aethese@users.noreply.github.com>
2024-08-15 02:42:40 -0500

    handle problems, add comments

--- main.ps1
+++ main.ps1
@@ -1,17 +1,38 @@
+# import ADC
 $scriptDir = Split-Path -parent $MyInvocation.MyCommand.Path
-Import-Module $scriptDir\Modules\AudioDeviceCmdlets\3.1.0.2\AudioDeviceCmdlets.psd1
+try
+{
+	Import-Module $scriptDir\Modules\AudioDeviceCmdlets\3.1.0.2\AudioDeviceCmdlets.psd1 -ErrorAction Stop
+}
+catch [System.IO.FileNotFoundException]
+{
+	Write-Warning "AudioDeviceCmdlets not found in path. Path checked: $scriptDir\Modules\AudioDeviceCmdlets\3.1.0.2\AudioDeviceCmdlets.psd1"
+	exit
+}
 
-$volumeLevel = Read-Host "Your preferred volume level (as a percentage)"
+# handle volume level
+$userInput = Read-Host "Your preferred volume level (as a percentage)"
+$volumeLevel = $userInput -as [int] # conver to int
 
+if ($volumeLevel -isnot [int])
+{
+	Write-Warning "Expected, but did not get, int for volume level"
+	exit
+}
+
+# get mic
 Write-Output "Select by Index which microphone you'd like to change:"
 Start-Sleep -Seconds 1
 Get-AudioDevice -List
 $index = Read-Host "Index"
 
-try {
+try
+{
 	$device = Get-AudioDevice -Index $index
 	$deviceID = $device.ID
-} catch [System.ArgumentException] {
+}
+catch [System.ArgumentException]
+{
 	Write-Warning "No device found at index $index"
 	exit
 }
@@ -19,12 +40,14 @@ try {
 Set-AudioDevice -Id $deviceID # select mic
 Set-AudioDevice -RecordingCommunicationVolume $volumeLevel # change volume
 
+# run loop
 $iteration = 0
 $runTime = 4 # in hours
 Write-Output "Setting microphone value to $volumeLevel% for $runTime hour(s). Press ctrl+c to stop the script early"
 
 # hours -> minutes -> ms
-while ($iteration -lt ($runTime * 60 / 0.5)) {
+while ($iteration -lt ($runTime * 60 / 0.5))
+{
 	Set-AudioDevice -RecordingCommunicationVolume $volumeLevel
 	$iteration += 1
 	Start-Sleep -Milliseconds 500
